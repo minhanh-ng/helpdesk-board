87a499794e1eeaf95786b32330b4a8b2b8de6898
Minh Anh Nguyen <minhanhn@usf.edu>
2025-11-09 14:23:46 -0500

    applied the same return as in status filter file

--- src/app/components/PriorityFilter.jsx
+++ src/app/components/PriorityFilter.jsx
@@ -3,5 +3,18 @@
 
 export default function PriorityFilter({ value, options = [], onChange }) {
     const priority = ['All', 'Low', 'Medium', 'High', 'Critical']
-    
+    return (
+    <label className="priority-filter-label">
+      <span className="priority">Status</span>
+      <select
+        className="select-priorityfilter"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      >
+        {options.map(opt => (
+          <option key={opt} value={opt}>{opt}</option>
+        ))}
+      </select>
+    </label>
+  );
 }
